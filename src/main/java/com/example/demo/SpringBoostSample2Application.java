f0d6f920725e3186acb7eb02a943d9a4adbae250
s2peachy <omgmilzy@gmail.com>
2022-10-19 02:06:57 +0900

    3.1HTMLの表示_動作確認

---
+++ src/main/java/com/example/demo/SpringBoostSample2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoostSample2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoostSample2Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/hello/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.demo.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+	
+	@GetMapping("/hello")
+	public String getHello() {
+		//hello.htmlに画面遷移
+		return "hello";
+	}
+
+}
