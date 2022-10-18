9a187b91cc4af75e36f3d2cced95ccfd472a8c04
s2peachy <omgmilzy@gmail.com>
2022-10-19 02:21:23 +0900

    3.2画面から別画面に値を渡す_動作確認

--- src/main/java/com/example/demo/hello/HelloController.java
+++ src/main/java/com/example/demo/hello/HelloController.java
@@ -1,7 +1,10 @@
 package com.example.demo.hello;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -10,6 +13,15 @@ public class HelloController {
 	public String getHello() {
 		//hello.htmlに画面遷移
 		return "hello";
+	}
+	
+	@PostMapping("/hello")
+	public String postRequest(@RequestParam("text1") String str, Model model) {
+		
+		//画面から受け取った文字列をModelに登録
+		model.addAttribute("sample", str);
+		//response.htmlに画面遷移
+		return "hello/response";
 	}
 
 }
