4849c590c028f3a98ab99abb7cf6136e11c50ca7
s2peachy <omgmilzy@gmail.com>
2022-10-21 02:00:19 +0900

    6.1.2メッセージプロパティ_動作確認

--- src/main/java/com/example/demo/application/service/UserApplicationService.java
+++ src/main/java/com/example/demo/application/service/UserApplicationService.java
@@ -1,18 +1,26 @@
 package com.example.demo.application.service;
 
 import java.util.LinkedHashMap;
+import java.util.Locale;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserApplicationService {
 	
+	@Autowired
+	private MessageSource messageSource;
+	
 	/**性別のMapを生成する*/
 	public Map<String, Integer> getGenderMap() {
 		Map<String, Integer> genderMap = new LinkedHashMap<>();
-		genderMap.put("男性", 1);
-		genderMap.put("女性", 2);
+		String male = messageSource.getMessage("male", null, Locale.JAPAN);
+		String female = messageSource.getMessage("female", null, Locale.JAPAN);
+		genderMap.put(male, 1);
+		genderMap.put(female, 2);
 		return genderMap;
 	}
 
