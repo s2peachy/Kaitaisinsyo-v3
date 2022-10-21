d860e9099dd07d01f319f18ae5d4c80853bfcee2
s2peachy <omgmilzy@gmail.com>
2022-10-22 02:18:49 +0900

    6.2.1バインド

---
+++ src/main/java/com/example/demo/form/SignupForm.java
@@ -0,0 +1,20 @@
+package com.example.demo.form;
+
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.Data;
+
+@Data
+public class SignupForm {
+	private String userId;
+	private String password;
+	private String userName;
+	@DateTimeFormat(pattern = "yyyy/MM/dd")
+	private Date birthday;
+	
+	private Integer age;
+	private Integer gender;
+
+}
