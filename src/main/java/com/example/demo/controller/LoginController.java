8d235555bd317b488f8d3e7eb05e32ed28e9f428
s2peachy <omgmilzy@gmail.com>
2022-10-27 01:13:45 +0900

    7.1質問用

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
@@ -10,6 +11,12 @@ public class LoginController {
 	@GetMapping("/login")
 	public String getLogin() {
 		return "login/login";
+	}
+	
+	/**ユーザー一覧画面にリダイレクト*/
+	@PostMapping("/login")
+	public String postLogin() {
+		return "redirect:/user/list";
 	}
 
 }
