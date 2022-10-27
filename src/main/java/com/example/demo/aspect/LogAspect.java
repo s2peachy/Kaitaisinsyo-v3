a8427fece8a7c404c020e8ac76d5faacb8ad8722
s2peachy <omgmilzy@gmail.com>
2022-10-28 04:16:37 +0900

    9.2.3Aroundの実装_動作確認

--- src/main/java/com/example/demo/aspect/LogAspect.java
+++ src/main/java/com/example/demo/aspect/LogAspect.java
@@ -1,7 +1,9 @@
 package com.example.demo.aspect;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -31,6 +33,33 @@ public class LogAspect {
 	@After("execution(* *..*.*UserService.*(..))")
 	public void endLog(JoinPoint jp) {
 		log.info("メソッド終了:" + jp.getSignature());
+	}
+	
+	/**コントローラーの実行前後にログ出力する*/
+	//@Around("bean(*Controller)")
+	//@Around("@annotation(org.springframework.web.bind.annotation.GetMapping)")
+	@Around("@within(org.springframework.stereotype.Controller)")
+	public Object startLog(ProceedingJoinPoint jp) throws Throwable {
+		
+		//開始ログ出力
+		log.info("メソッド開始:" + jp.getSignature());
+		
+		try {
+			//メソッド実行
+			Object result = jp.proceed();
+			
+			//終了ログ出力
+			log.info("メソッド終了:" + jp.getSignature());
+			
+			//実行結果を呼び出し元に返却
+			return result;
+		} catch(Exception e) {
+			//エラーログ出力
+			log.error("メソッド異常終了:" + jp.getSignature());
+			
+			//エラーの再スロー
+			throw e;
+		}
 	}
 
 }
