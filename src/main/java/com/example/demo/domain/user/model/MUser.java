7aef0be1d9088d08ce95efe8bd58815d11cddc3c
s2peachy <omgmilzy@gmail.com>
2022-11-05 00:56:40 +0900

    13.3.2テーブル結合(多対一)_動作確認

--- src/main/java/com/example/demo/domain/user/model/MUser.java
+++ src/main/java/com/example/demo/domain/user/model/MUser.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
@@ -24,7 +26,8 @@ public class MUser {
 	private Integer gender;
 	private Integer departmentId;
 	private String role;
-	@Transient
+	@ManyToOne(optional = true)
+	@JoinColumn(insertable=false, updatable=false, name="departmentId")
 	private Department department;
 	@Transient
 	private List<Salary> salaryList;
