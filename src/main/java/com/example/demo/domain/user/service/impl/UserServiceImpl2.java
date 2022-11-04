65453e94dda17f4cdadabd84a253815780e63116
s2peachy <omgmilzy@gmail.com>
2022-11-05 00:43:30 +0900

    13.2.2任意のSQL_動作確認

--- src/main/java/com/example/demo/domain/user/service/impl/UserServiceImpl2.java
+++ src/main/java/com/example/demo/domain/user/service/impl/UserServiceImpl2.java
@@ -65,6 +65,12 @@ public class UserServiceImpl2 implements UserService {
 	@Override
 	public void updateUserOne(String userId, String password, String userName) {
 		
+		//パスワード暗号化
+		String encryptPassword = encoder.encode(password);
+		
+		//ユーザー更新
+		repository.updateUser(userId, encryptPassword, userName);
+		
 	}
 	
 	/**ユーザー削除(1件)*/
@@ -77,9 +83,7 @@ public class UserServiceImpl2 implements UserService {
 	/**ログインユーザー取得*/
 	@Override
 	public MUser getLoginUser(String userId) {
-		Optional<MUser> option = repository.findById(userId);
-		MUser user = option.orElse(null);
-		return user;
+		return repository.findLoginUser(userId);
 	}
 
 }
