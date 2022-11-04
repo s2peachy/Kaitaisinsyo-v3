eee3bfe64cf5c2c6983ce2167221f3c685d7e49e
s2peachy <omgmilzy@gmail.com>
2022-11-04 22:49:51 +0900

    12.2.3検索_動作確認

--- src/main/java/com/example/demo/rest/UserRestController.java
+++ src/main/java/com/example/demo/rest/UserRestController.java
@@ -1,6 +1,7 @@
 package com.example.demo.rest;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
@@ -11,6 +12,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +23,7 @@ import com.example.demo.domain.user.service.UserService;
 import com.example.demo.form.GroupOrder;
 import com.example.demo.form.SignupForm;
 import com.example.demo.form.UserDetailForm;
+import com.example.demo.form.UserListForm;
 
 @RestController
 @RequestMapping("/user")
@@ -34,6 +37,18 @@ public class UserRestController {
 	
 	@Autowired
 	private MessageSource messageSource;
+	
+	/**ユーザーを検索*/
+	@GetMapping("/get/list")
+	public List<MUser> getUserList(UserListForm form) {
+		
+		//formをMUserクラスに変換
+		MUser user = modelMapper.map(form, MUser.class);
+		
+		//ユーザー一覧取得
+		List<MUser> userList = userService.getUsers(user);
+		return userList;
+	}
 	
 	/**ユーザーを登録*/
 	@PostMapping("/signup/rest")
